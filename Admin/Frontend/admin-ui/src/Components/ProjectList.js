407e8b501e5af11bb80a3a42cdb0420e545e562c
prathas24 <prathasawant21@gmail.com>
2024-10-20 11:52:02 +0530

    Update ProjectList.js

--- Admin/Frontend/admin-ui/src/Components/ProjectList.js
+++ Admin/Frontend/admin-ui/src/Components/ProjectList.js
@@ -6,7 +6,7 @@ const ProjectList = () => {
 
   const fetchProjects = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/projects');
+      const response = await axios.get('https://line-and-design.onrender.com/projects');
       setProjects(response.data);
     } catch (error) {
       console.error('Error fetching projeacts:', error);
